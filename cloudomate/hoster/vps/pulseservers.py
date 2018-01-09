c26dd77c8e10cda9ca6fbc2a557919bf42328cb1
TheFrog <aizokroon@hotmail.com>
2018-01-09 15:04:46 +0100

    Pulseserver (1/2)

--- cloudomate/hoster/vps/pulseservers.py
+++ cloudomate/hoster/vps/pulseservers.py
@@ -6,32 +6,135 @@ from cloudomate.hoster.vps.clientarea import ClientArea
 from cloudomate.hoster.vps.vpsoption import VpsOption
 from cloudomate.wallet import determine_currency
 
+from cloudomate.hoster.vps import vps_hoster
+import sys
+
 
 class Pulseservers(SolusvmHoster):
-    """
-    PulseServers contains the logic to view hosting configurations and to 
-    purchase servers at Pulseservers.
-    """
-    name = "pulseservers"
-    website = "https://pulseservers.com/"
-    required_settings = [
-        'firstname',
-        'lastname',
-        'email',
-        'phonenumber',
-        'address',
-        'city',
-        'state',
-        'zipcode',
-        'password',
-        'hostname',
-        'rootpw'
-    ]
     clientarea_url = 'https://www.pulseservers.com/billing/clientarea.php'
-    gateway = coinbase
 
-    def __init__(self, settings):
-        super(Pulseservers, self).__init__(settings)
+
+
+    OPTIONS_URL = 'https://pulseservers.com/vps-linux.html'
+    CART_URL = 'https://www.pulseservers.com/billing/cart.php?a=confdomains'
+
+
+
+    '''
+    Information about the Hoster
+    '''
+
+    @staticmethod
+    def get_gateway():
+        return coinbase
+
+    @staticmethod
+    def get_metadata():
+        return ('PulseServers', 'https://pulseservers.com')
+
+    @staticmethod
+    def get_required_settings():
+        return {
+            'user': ['firstname', 'lastname', 'email', 'phonenumber', 'address', 'city', 'state', 'zipcode', 'password'],
+            'server': ['hostname', 'rootpw']
+        }
+
+
+    '''
+    Action methods of the Hoster that can be called
+    '''
+
+    def get_configuration(self):
+        pass
+
+    @classmethod
+    def get_options(cls):
+        browser = cls._create_browser()
+        browser.open(cls.OPTIONS_URL)
+
+        # Get all pricing boxes
+        soup = browser.get_current_page()
+        boxes = soup.select('div.pricing-box')
+        return [cls._parse_box(box) for box in boxes]
+
+    def get_status(self):
+        pass
+
+    def purchase(self, wallet, option):
+        self._browser.open(option.purchase_url)
+        self._fill_server_form()
+        self._browser.open(self.CART_URL)
+        page = self._fill_user_form()
+        self.pay(self.get_gateway(), page.url)
+
+    def set_root_password(self, password):
+        pass
+
+
+    '''
+    Hoster-specific methods that are needed to perform the actions
+    '''
+
+    def _fill_server_form():
+        self.server_form(self._settings)
+
+    def _fill_user_form():
+        # Select the correct submit button
+        form = self._browser.select_form('form#mainfrm')
+        soup = self._browser.get_current_page()
+        submit = soup.select_one('input.ordernow')
+        form.choose_submit(submit)
+
+        # Let SolusVM class handle the rest
+        gateway = self.get_gateway()
+        self.user_form(self._browser, self._settings, gateway.name, errorbox_class='errorbox')
+
+        # Redirect to Coinbase
+        self._browser.select_form(nr=0)
+        return self._browser.submit_selected()
+
+    @staticmethod
+    def _parse_box(box):
+        details = box.findAll('li')
+
+        name = details[0].h4.text
+
+        price = details[1].h1.text
+        price = float(price[1:])
+
+        cores = details[2].strong.text
+        cores = int(cores.split(' ')[0])
+
+        memory = details[3].strong.text
+        memory = float(memory[0:-2])
+
+        storage = details[4].strong.text
+        if storage == '1TB':
+            storage = 1000.0
+        else:
+            storage = float(storage[0:-2])
+
+        connection = details[5].strong.text
+        connection = int(connection[0:-7])
+
+        purchase_url = details[9].a['href']
+
+        return vps_hoster.VpsOption(name, cores, memory, storage, sys.maxsize, connection, price, purchase_url)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#    def __init__(self, settings):
+#        super(Pulseservers, self).__init__(settings)
 
     def start(self):
         """
@@ -51,44 +154,18 @@ class Pulseservers(SolusvmHoster):
         self.configurations = [self._parse_box(box) for box in pricingboxes]
         return self.configurations
 
-    @staticmethod
-    def _parse_box(box):
-        """
-        Parse a single hosting configuration
-        :param box: Div containing hosting details
-        :return: VpsOption containing hosting details
-        """
-        details = box.findAll('li')
-        storage = details[4].strong.text
-        if storage == '1TB':
-            storage = 1024.0
-        else:
-            storage = float(storage.split('G')[0])
 
-        connection = details[5].strong.text.split('G')[0]
-        connection = int(connection) * 1000
-        return VpsOption(
-            name=details[0].h4.text,
-            price=float(details[1].h1.text.split('$')[1]),
-            currency=determine_currency(details[1].h1.text),
-            cpu=int(details[2].strong.text.split('C')[0]),
-            ram=float(details[3].strong.text.split('G')[0]),
-            storage=storage,
-            connection=connection,
-            bandwidth='unmetered',
-            purchase_url=details[9].a['href']
-        )
 
-    @staticmethod
-    def _beautify_cpu(cores, speed):
-        """
-        Format cores and speed to fit cpu column
-        :param cores: cores text
-        :param speed: speed text
-        :return: formatted string
-        """
-        spl = cores.split()
-        return '{0}c/{1}t {2}'.format(spl[0], spl[3], speed[:-4])
+    # @staticmethod
+    # def _beautify_cpu(cores, speed):
+    #     """
+    #     Format cores and speed to fit cpu column
+    #     :param cores: cores text
+    #     :param speed: speed text
+    #     :return: formatted string
+    #     """
+    #     spl = cores.split()
+    #     return '{0}c/{1}t {2}'.format(spl[0], spl[3], speed[:-4])
 
     def register(self, user_settings, vps_option):
         """
