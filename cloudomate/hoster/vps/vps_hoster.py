e4a9ec2eeebf926d0eb87e8583ba39230ce8a24b
Tom Catshoek <t.w.j.catshoek@student.tudelft.nl>
2018-01-09 12:52:28 +0100

    tuple formatting

--- cloudomate/hoster/vps/vps_hoster.py
+++ cloudomate/hoster/vps/vps_hoster.py
@@ -3,8 +3,21 @@ from collections import namedtuple
 
 
 VpsConfiguration = namedtuple('VpsConfiguration', ['ip', 'password'])
-VpsOption = namedtuple('VpsOption', ['cores', 'memory', 'storage', 'bandwidth', 'connection', 'price', 'purchase_url'])  # Price in USD, storage and memory in GB 
-VpsStatus = namedtuple('VpsStatus', ['memory_used', 'storage_used', 'bandwidth_used', 'online', 'expiration'])   # Online is a boolean, expiration an ISO datetime
+
+
+VpsOption = namedtuple('VpsOption', ['cores',
+                                     'memory',          # Memory in GB
+                                     'storage',         # Storage in GB
+                                     'bandwidth',
+                                     'connection',
+                                     'price',           # Price in USD
+                                     'purchase_url'])
+
+VpsStatus = namedtuple('VpsStatus', ['memory_used',
+                                     'storage_used',
+                                     'bandwidth_used',
+                                     'online',          # Boolean
+                                     'expiration'])     # ISO datetime
 
 
 class VpsHoster(Hoster):
